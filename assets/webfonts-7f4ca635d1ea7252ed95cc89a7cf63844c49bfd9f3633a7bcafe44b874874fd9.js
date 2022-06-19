b4d6f0fad2262fa9ff07cec0b79edd967ef27b6b
Faraaz Khan <faraazkh@amazon.com>
2022-06-19 00:56:41 -0700

    [ci skip] [ci skip]

--- assets/webfonts-7f4ca635d1ea7252ed95cc89a7cf63844c49bfd9f3633a7bcafe44b874874fd9.js
+++ assets/webfonts-7f4ca635d1ea7252ed95cc89a7cf63844c49bfd9f3633a7bcafe44b874874fd9.js
@@ -19,6 +19,6 @@ g,0<d.length&&(d=za[d[0]])&&(a.c[e]=d))}a.c[e]||(d=za[e])&&(a.c[e]=d);for(d=0;d<
 
 WebFont.load({
   google: {
-    families: ['Cormorant Garamond:700', 'Lato:300,400,700']
+    families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Inconsolata:300,400,700', 'Source Sans Pro:300,400,700', 'Open Sans:800']
   }
 });
