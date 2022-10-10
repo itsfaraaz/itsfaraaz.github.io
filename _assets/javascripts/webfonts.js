ef5e4eca28a2a951cc0fc49decc2283a44c7d14d
Faraaz Khan <faraazkhan212@gmail.com>
2022-10-09 23:56:30 -0400

    added changing friendship post [ci skip]

--- _assets/javascripts/webfonts.js
+++
@@ -1,7 +0,0 @@
-//= require webfontloader/webfontloader.js
-
-WebFont.load({
-  google: {
-    families: ['Cormorant Garamond:700', 'Lato:300,400,700', 'Inconsolata:300,400,700', 'Source Sans Pro:300,400,700', 'Open Sans:800']
-  }
-});
